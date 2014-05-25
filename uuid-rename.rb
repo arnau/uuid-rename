a695bbe0e571e5c383c41c6681f7d449c6594d7f
Arnau Siches <asiches@gmail.com>
2014-05-25 12:00:10 +0100

    Remove backup actions

--- uuid-rename.rb
+++ uuid-rename.rb
@@ -1,9 +1,5 @@
 require 'pathname'
 require 'securerandom'
-require 'fileutils'
-
-bak_dir = "#{ENV['HOME']}/uuid-rename.bak"
-FileUtils.mkdir_p(bak_dir)
 
 ARGV.each do |item|
   path = Pathname(item)
@@ -11,8 +7,6 @@ ARGV.each do |item|
   dt = (Time.now.utc.to_f * 1000000).to_i
   file = path.expand_path
   uuid_path = file.dirname.join("#{dt}-#{uuid}#{file.extname}")
-
-  FileUtils.cp(file, bak_dir)
 
   file.rename(uuid_path)
 end
